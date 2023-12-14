77961de0923afb955df3a84fa0ddcf90da1838db
Lucas Ricardo <lucasricardobarb@gmail.com>
2023-12-13 23:42:24 -0300

    adding Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { Post } from "./Post";
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+
+import "./global.css";
 
 export function App() {
   return (
     <div>
-      <Post 
-        author="Diego Fernandes" 
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, animi a? Totam odio autem molestias doloribus aspernatur minima, delectus facere nobis repudiandae cum iste, ex sunt sed eius ducimus exercitationem!" 
-      />
+      <Header />
 
-      <Post 
-        author="Lucas Ricardo"
-        content="Estudante Front-End"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quaerat deserunt optio expedita eius doloribus aspernatur alias asperiores, id mollitia debitis provident neque, pariatur quibusdam eveniet ab iste cum reprehenderit."
+          />
+           <Post
+            author="Diego Fernandes"
+            content="Estudante Front-End"
+          />
+        </main>
+      </div>
     </div>
   );
 }
